04a28752804bfc45102085f878d6de8afd4c03bc
Brennan <n3rdtp@gmail.com>
2023-07-30 11:59:21 -0500

    added handlebars and test page

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const handlebars = require('express-handlebars');
 const app = express();
 
-app.use(express.static(__dirname + '/client'))
+app.use(express.static(__dirname + '/public'))
+app.set("views", __dirname + "/views")
+app.set('view engine', 'handlebars');
+app.engine('handlebars', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    defaultView: 'home'
+}));
 
-const port = process.env.PORT || 3000
-app.get('/test', function(request, response) {
-	response.type('text/plain')
-	response.send('Node.js and Express running on port='+port)
-})
+const PORT = process.env.PORT || 3000
 
-app.listen(port, function() {
+app.get('/', function(request, response) {
+	response.render()
+});
+
+// app.get('/test', function(request, response) {
+// 	response.type('text/plain')
+// 	response.send('Node.js and Express running on port='+ PORT)
+// });
+
+app.get('/test', (req, res) => res.render('home', {layout: 'index'}));
+
+app.listen(PORT, function() {
 	console.log("Server is running at http://localhost:3000/")
-})
+});
